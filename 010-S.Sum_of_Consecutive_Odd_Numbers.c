5f7a9d5e17f07720c4a11931f01bd5ffcb4a4a6e
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-04-09 23:47:02 +0600

    problem(008 to 011): 4 different problems solved

---
+++ 010-S.Sum_of_Consecutive_Odd_Numbers.c
@@ -0,0 +1,36 @@
+/**
+ * https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/S
+ * 
+ * S. Sum of Consecutive Odd Numbers
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int testCase;
+    scanf("%d", &testCase);
+    for (int j = 1; j <= testCase; j++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        if(x > y){
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sum = 0;
+            for (int i = x + 1; i < y; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum = sum + i;
+                }
+            }
+     
+        printf("%d\n", sum);
+    }
+    return 0;
+}
---
+++ 011-G.Factorial.c
@@ -0,0 +1,22 @@
+/**
+ * https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
+ * 
+ * Given a number N. Print the factorial of number N.
+ */
+
+#include <stdio.h>
+
+int main(){
+    int testCase;
+    scanf("%d", &testCase);
+    for(int i = 1; i<=testCase; i++){
+        int num;
+        scanf("%d", &num);
+        long long result = 1;
+        for(int j = num; j>=1; j--){
+            result = result * j;
+        }
+        printf("%lld\n", result);
+    }
+    return 0;
+}
